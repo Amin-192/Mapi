39eea4431acd9c5087b82743077d66a670ad93df
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-11-11 16:53:09 +0300

    easy

--- mapi/src/app/pages/About/page.jsx
+++ mapi/src/app/pages/About/page.jsx
@@ -1,9 +1,26 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
 
 export default function page() {
+  const [input, setInput] = useState('')
+  const [list, setList] = useState([]);
+  const handleSave =()=>{
+   setList([...list , input])
+
+  }
+  
   return (
     <div>
-      About page 
+      
+      <input type='text' className='p-2 border-green-300 ' onChange={(ev)=> setInput(ev.target.value)} />
+      <button className='p-5 rounded-2xl bg-white ml-7' onClick={handleSave}>Save</button>
+      <h1>{input}</h1>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   )
 }
