87c5522e427a2f827110977796e49afec339bd3e
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-11-19 14:27:00 +0300

    small change

--- prompty/src/app/components/Feed.jsx
+++ prompty/src/app/components/Feed.jsx
@@ -4,8 +4,10 @@ import { useState, useEffect } from 'react'
 import promptCard from '../components/PromptCard'
 export default function Feed() {
   return (
-    <div>
+    <section className=''>
+      <form className='relative w-full flex-container'
       
-    </div>
+      />
+    </section>
   )
 }
