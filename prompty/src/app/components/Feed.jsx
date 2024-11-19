b531441c92e58d0dd55e0f55f2ece9ac5d5f8bbd
Amin Hassan <125133476+Amin-192@users.noreply.github.com>
2024-11-19 14:06:36 +0300

    slite change

--- prompty/src/app/components/Feed.jsx
+++ prompty/src/app/components/Feed.jsx
@@ -1,5 +1,7 @@
+'use client'
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import promptCard from '../components/PromptCard'
 export default function Feed() {
   return (
     <div>
